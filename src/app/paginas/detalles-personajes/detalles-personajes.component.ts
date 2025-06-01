6508dda74f539f6e811c2d809cfea40276804799
lucianaaaaaaa <luciana.leano@ucb.edu.bo>
2025-06-01 19:42:56 -0400

    detalles

---
+++ src/app/paginas/detalles-personajes/detalles-personajes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalles-personajes',
+  imports: [],
+  templateUrl: './detalles-personajes.component.html',
+  styleUrl: './detalles-personajes.component.scss'
+})
+export class DetallesPersonajesComponent {
+
+}
